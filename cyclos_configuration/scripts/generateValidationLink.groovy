e54c164011cde39e49aee75b0613e2bf93606bd5
Sandra Both <sandrab.git@gmail.com>
2018-10-09 09:55:48 +0200

    Fix link generation script for Cyclos 4.10

--- cyclos_configuration/scripts/generateValidationLink.groovy
+++ cyclos_configuration/scripts/generateValidationLink.groovy
@@ -1,24 +1,22 @@
 /**
  * Generates a custom validation URL. Used in the e-mail Cyclos sends new users to ask them to validate their account.
- * @todo: from Cyclos 4.10 and up we can use a 'type' and 'validationKey' variable in this script instead of parsing the urlFilePart.
  */
 import org.cyclos.entities.users.SystemRecord
 import org.cyclos.entities.users.SystemRecordType
+import org.cyclos.impl.utils.LinkType
 import org.cyclos.model.users.records.RecordDataParams
 import org.cyclos.model.users.recordtypes.RecordTypeVO
 
 // Only generate a custom link if this is the validation link.
-if(urlFilePart == null) {
+if(type != LinkType.REGISTRATION_VALIDATION) {
 	return null
 }
-int indexKey = urlFilePart.indexOf("validationKey=")
-if (indexKey <= 0) {
+// We are supposed to have a validationKey in this case.
+if(validationKey?.empty) {
     return null
 }
 
 // Build up the custom URL and return this.
-String validationKeyParam = urlFilePart.split('validationKey=')[1]
-
 String recordTypeName = 'mollyConnect'
 String fieldName = 'registrationRootUrl'
 SystemRecordType recordType = binding.entityManagerHandler.find(SystemRecordType, recordTypeName)
@@ -27,4 +25,4 @@ if (!record.persistent) throw new IllegalStateException("No instance of system r
 wrapped = binding.scriptHelper.wrap(record, recordType.fields)
 if (!wrapped.containsKey(fieldName)) throw new Exception("FieldName ${fieldName} is missing in ${recordTypeName} system record.")
 
-return wrapped[fieldName] + scriptParameters.validationURL + "?validationKey=${validationKeyParam}"
+return wrapped[fieldName] + scriptParameters.validationURL + "?validationKey=${validationKey}"
