cab7a0aed81afc8fd598cbc775b5d7fd78b1995d
Sandra Both <sandrab.git@gmail.com>
2018-11-27 13:59:59 +0100

    Add field to decide whether alternative payment validation script should accept agreements
    Fixes #34

--- cyclos_configuration/scripts/alternativePaymentValidation.groovy
+++ cyclos_configuration/scripts/alternativePaymentValidation.groovy
@@ -4,10 +4,10 @@ def usrDTO = userService.load(user.id)
 def usr = scriptHelper.wrap(usrDTO)
 
 // Check if the given iban and e-mail match those in the cyclos user profile.
-if (formParameters.email != user.email){
+if (formParameters.email != user.email) {
     throw new ValidationException("Het ingevoerde e-mailadres (${formParameters.email}) komt niet overeen met het e-mailadres van deze gebruiker in Cyclos (${user.email}).")
 }
-if(!usr.iban.replace(" ","").equalsIgnoreCase(formParameters.iban.replace(" ", ""))){
+if (!usr.iban.replace(" ","").equalsIgnoreCase(formParameters.iban.replace(" ", ""))) {
     throw new ValidationException("Het ingevoerde IBAN (${formParameters.iban}) komt niet overeen met het IBAN van deze gebruiker in Cyclos (${usr.iban}). Controleer of je het IBAN correct hebt ingevuld of, als het IBAN in Cyclos niet juist is, pas dan eerst het IBAN van deze gebruiker in Cyclos aan in het geverifieerde IBAN voordat je deze operatie laat uitvoeren.")
 }
 
@@ -16,7 +16,7 @@ BigDecimal contribution = utils.getLidmaatschapsbijdrage(usr)
 BigDecimal aankoop_saldo = (usr.aankoop_saldo?:0)
 BigDecimal totalAmount = contribution + aankoop_saldo
 
-if(formParameters.amount != totalAmount){
+if (formParameters.amount != totalAmount) {
     def amountEntered = formatter.format(formParameters.amount)
     def amountCyclos = formatter.format(totalAmount)
     throw new ValidationException("Het ingevoerde bedrag (${amountEntered}) komt niet overeen met wat Cyclos voor deze gebruiker verwacht (${amountCyclos}).")
@@ -34,8 +34,10 @@ userService.save(usrDTO)
 // Call userService to validate the user.
 userValidationService.manuallyValidateRegistration(new UserLocatorVO(user.id))
 
-// Accept all personal agreements.
-utils.acceptAgreements(user)
+// Accept all personal agreements if the admin confirmed the user has done this.
+if (formParameters.agreements_accepted) {
+    utils.acceptAgreements(user)
+}
 
 // Create the transactions.
 String method = formParameters.method.internalName
