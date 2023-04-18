a9a46c5f4a2a29d432dfeabd1e02a9f5f6d34f79
Sandra Both <sandrab.git@gmail.com>
2023-04-18 12:43:25 +0200

    Only update IBAN from emandate for existing users

--- cyclos_configuration/scripts/emandates/Operation_InternalCreate_Callback.groovy
+++ cyclos_configuration/scripts/emandates/Operation_InternalCreate_Callback.groovy
@@ -14,5 +14,10 @@ def eMandates = new EMandates(binding)
 def storage = new EntityBackedParameterStorage(objectMapper, execution)
 
 def fields = eMandates.callback(storage, transactionId)
+
+// Update the IBAN in the user profile.
+eMandates.updateUserIBAN(fields)
+
+// Inform the user about the result.
 String status = (fields.status as CustomFieldPossibleValue).internalName
 return new Utils(binding).dynamicMessage("emResult${status.capitalize()}")
