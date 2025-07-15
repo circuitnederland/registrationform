e98e011832b4cc3281fe4057eace95e59f1f477c
Sandra Both <sandrab.git@gmail.com>
2025-07-15 12:00:34 +0200

    Fix webservice skipping steps for Cyclos 4.16 changes

--- cyclos_configuration/scripts/registration/WebService_PrepareRegistrationWizard.groovy
+++ cyclos_configuration/scripts/registration/WebService_PrepareRegistrationWizard.groovy
@@ -6,9 +6,13 @@
 // If community is known, the wizard is set to go from step intro to step type, skipping the community step.
 // If both community and type are known, the wizard is set to go from step intro to step email, skipping both community and type steps.
 import org.apache.http.HttpStatus
+import org.cyclos.entities.system.CustomWizard
+import org.cyclos.entities.system.CustomWizardExecution
 import org.cyclos.impl.access.SessionData
+import org.cyclos.impl.system.CustomWizardExecutionStorage
 import org.cyclos.impl.system.CustomWizardServiceLocal
 import org.cyclos.impl.system.ScriptHelper
+import org.cyclos.model.system.wizards.CustomWizardExecutionData
 import org.cyclos.model.system.wizards.CustomWizardVO
 import org.cyclos.model.system.wizards.StartCustomWizardParams
 import org.cyclos.model.utils.ResponseInfo
@@ -18,12 +22,12 @@ ScriptHelper scriptHelper = binding.scriptHelper
 SessionData sessionData = binding.sessionData
 
 // Start a wizard execution
-def run = customWizardService.start(new StartCustomWizardParams(
+CustomWizardExecutionData run = customWizardService.start(new StartCustomWizardParams(
         wizard: new CustomWizardVO(internalName: 'registration')))
 def key = run.key
-def execution = customWizardService.findExecution(key)
-def storage = execution.storage
-def wizard = execution.wizard
+CustomWizardExecution execution = customWizardService.findExecution(key) as CustomWizardExecution
+CustomWizardExecutionStorage storage = execution.storage as CustomWizardExecutionStorage
+CustomWizard wizard = execution.wizard as CustomWizard
 
 // Find the chosen values of the custom wizard fields based on the community and type in the request URL (they may or may not exist).
 def communityField = wizard.customFields.find { it.internalName == 'community'}
@@ -41,8 +45,7 @@ storage.customValues = customValues
 // If the type is also known, go directly to the email step. If not, go to the type step.
 if (communityValue) {
     def steps = storage.steps
-    def nextStep = typeValue ? 'email' : 'type'
-    steps[0].transitions[0].step = wizard.steps.find { it.internalName == nextStep}
+    steps[0].transitions[0].id = typeValue ? 'email' : 'type'
     storage.steps = steps
 }
 
